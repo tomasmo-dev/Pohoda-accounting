7a60fd600aa1af27ab8345e8bae81217cd3065c4
moravec.tomas <moravec.tomas@ssakhk.cz>
2023-07-06 20:37:21 +0200

    query edit

--- queries.php
+++ queries.php
@@ -5,13 +5,13 @@
         $CustIds = array();
 
         $sql = 
-        "SELECT DISTINCT d.cust_ID, MONTH(d.dispend) AS month, YEAR(d.dispend) AS year, d.dispapt
-            FROM system.qb_revenue_items i
-                LEFT JOIN myfbo_cz_copy.dispatches d ON d.dispatch_ID = i.dispatch_ID AND d.ddel = 0
-            WHERE business_unit = 'CZ'
-                AND MONTH(dispend) = ? AND YEAR(dispend) = ?
-                AND i.item_type NOT IN ('FX', 'H', 'X', 'Z')
-                GROUP BY d.cust_ID, d.dispapt;";
+        'SELECT DISTINCT d.cust_ID, MONTH(d.dispend) AS month, YEAR(d.dispend) AS year, d.dispapt '.
+            'FROM system.qb_revenue_items i '.
+                'LEFT JOIN myfbo_cz_copy.dispatches d ON d.dispatch_ID = i.dispatch_ID AND d.ddel = 0 '.
+            'WHERE business_unit = \'CZ\' '.
+                'AND MONTH(dispend) = ? AND YEAR(dispend) = ? '.
+                'AND i.item_type NOT IN (\'FX\', \'H\', \'X\', \'Z\') '.
+                'GROUP BY d.cust_ID, d.dispapt; ';
 
         echo $sql;
 
