666a7b27d421fb5a7bb86dec45e21a67d96e81ad
moravec.tomas <moravec.tomas@ssakhk.cz>
2023-07-06 18:48:14 +0200

    debug

--- queries.php
+++ queries.php
@@ -21,7 +21,9 @@
         $stmt->execute();
 
         $result = $stmt->get_result();
-        echo $result->num_rows;
+
+        echo "`$result->num_rows`";
+        
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
                 array_push($CustIds, $row["cust_ID"]);
