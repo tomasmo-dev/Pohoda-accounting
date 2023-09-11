91c526905d3e224b9d1ab4186b770585795a32d1
moravec.tomas <moravec.tomas@ssakhk.cz>
2023-09-11 15:54:14 +0200

    test2

--- ms_queries.php
+++ ms_queries.php
@@ -8,7 +8,25 @@ function SelectVarsym($varsym, $bal, $amount)
 {
     $ms_con = $GLOBALS['ms_con'];
 
-    
+    $sql = "SELECT * FROM FA WHERE Varsym = :varsym AND Bal = :bal AND Amount = :amount";
+
+    $stmt = $ms_con->prepare($sql);
+
+    $stmt->execute([
+        'varsym' => $varsym,
+        'bal' => $bal,
+        'amount' => $amount
+    ]);
+
+    $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+    if ($row) {
+        return $row;
+    } else {
+        return false;
+    }
 }
 
 ?>
--- test.php
+++ test.php
@@ -8,11 +8,21 @@
 
     $stmt->execute();
 
-    echo "Query: " . $sql . "<br><pre>";
+    echo "Query stmt normal: " . $sql . "<br><pre>";
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         print_r($row);
         echo "\n";
     }
-    echo "</pre>";
+    echo "</pre><br><br>";
+
+    $stmt = $ms_con->prepare($sql);
+
+    $stmt->execute();
+
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "Query stmt fetchAll: " . $sql . "<br><pre>";
+    print_r($data);
+    echo "</pre><br><br>";
 
 ?>
