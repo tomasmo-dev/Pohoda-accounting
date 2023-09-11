ec32350eeb03afa52be7774cb00745dd90d22f95
moravec.tomas <moravec.tomas@ssakhk.cz>
2023-09-12 01:59:58 +0200

    .

--- index.php
+++ index.php
@@ -140,7 +140,7 @@
                 $balance = $info['PrepayBalance']; // balance for client
 
                 // gets the invoice date from pohoda mssql server
-                $internal_inv_date = SelectVarsym($GLOBALS['ms_con'], $row['variable_symbol'], $balance, $total_price)[0]['Datum'];
+                $internal_inv_date = SelectVarsym($GLOBALS['ms_con'], $row['variable_symbol'], $balance, $total_price)['Datum'];
 
                     
                 if ($balance < 0 && abs($balance) > $internal_price)
